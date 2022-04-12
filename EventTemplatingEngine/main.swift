1dac324714a66fccff34614f1d2835363037b201
Brice Pollock <brice.pollock@gmail.com>
2022-04-12 13:39:35 -0700

    use correct file extensions for android

--- EventTemplatingEngine/main.swift
+++ EventTemplatingEngine/main.swift
@@ -8,6 +8,25 @@
 import Foundation
 import Mustache
 
+enum TemplateType: String {
+    case ios
+    case android
+    
+    var outputDirectory: String {
+        switch self {
+        case .ios: return "ios"
+        case .android: return "android"
+        }
+    }
+    
+    var fileExtension: String {
+        switch self {
+        case .ios: return ".swift"
+        case .android: return ".kt"
+        }
+    }
+}
+
 // CHANGE THIS VALUE TO WHERE YOU CLONED REPO
 let homePathComponents = "/src/tmp/templated_eventing"
 // --
@@ -31,20 +50,29 @@ do {
             // Perform json operations such as adding new template key-values
             let updatedNames = (json["cases"] as! [[String: Any]]).map { nameDictionary -> [String: Any] in
                 var newDictionary = nameDictionary
-                newDictionary["case_name"] = ((nameDictionary["name"] as? String) ?? "").replacingOccurrences(of: ".", with: "_").camelCased(with: "_")
+                let snakeCaseName = ((nameDictionary["name"] as? String) ?? "").replacingOccurrences(of: ".", with: "_")
+                let caseName = snakeCaseName.camelCased(with: "_")
+                newDictionary["case_name"] = caseName
+                newDictionary["case_name_caps"] = snakeCaseName.uppercased()
                 return newDictionary
             }
             json["cases"] = updatedNames
-
+            json["parameter_capitalized"] = (json["parameter"] as! String).camelCased(with:"_").capitalized
+            
             // Get all templates to apply to event files
             try fileManager.contentsOfDirectory(atPath: templateFilesHome)
-                .map { templateFile in templateFilesHome + "/" + templateFile }
-                .forEach { templateFilePath in
+                .forEach { templateFile in
+                    let templateFilePath = templateFilesHome + "/" + templateFile
                     let template = try Template(path: templateFilePath)
                     let rendering = try template.render(json)
                     
                     // Write template outputs to disk
-                    let outputFilePath = outputFilesHome + "/" + String(eventFile.dropLast(".json".count)) + ".swift"
+                    let platform = String(templateFile.dropLast(".txt".count))
+                    guard let templateType = TemplateType(rawValue: platform) else {
+                        throw NSError(domain: "bad_enum", code: 101)
+                    }
+                    let outputFilePath = outputFilesHome + "/\(templateType.outputDirectory)/" + String(eventFile.dropLast(".json".count)) + templateType.fileExtension
+                    
                     try rendering.write(toFile: outputFilePath, atomically: true, encoding: .unicode)
                 }
         }
